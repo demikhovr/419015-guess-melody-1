58b2605a1186e1b8f1622f0163fdb30525c231b8
demikhovr <demikhovr@gmail.com>
2019-05-12 23:25:19 +0300

    add questions mock

---
+++ src/mocks/questions.js
@@ -0,0 +1,50 @@
+export const settings = {
+  gameTime: 7,
+  errorCount: 4,
+};
+
+export default [
+  {
+    type: `genre`,
+    genre: `rock`,
+    answers: [
+      {
+        src: `https://upload.wikimedia.org/wikipedia/commons/1/1f/Uganda_flag_and_national_anthem_-_Oh_Uganda_Land_o.ogg`,
+        genre: `rock`,
+      },
+      {
+        src: `https://upload.wikimedia.org/wikipedia/commons/1/1f/Uganda_flag_and_national_anthem_-_Oh_Uganda_Land_o.ogg`,
+        genre: `blues`,
+      },
+      {
+        src: `https://upload.wikimedia.org/wikipedia/commons/1/1f/Uganda_flag_and_national_anthem_-_Oh_Uganda_Land_o.ogg`,
+        genre: `jazz`,
+      },
+      {
+        src: `https://upload.wikimedia.org/wikipedia/commons/1/1f/Uganda_flag_and_national_anthem_-_Oh_Uganda_Land_o.ogg`,
+        genre: `pop`,
+      },
+    ],
+  },
+  {
+    type: `artist`,
+    song: {
+      artist: `Jim Beam`,
+      src: `path.mp3`,
+    },
+    answers: [
+      {
+        picture: `path.jpg`,
+        artist: `John Snow`,
+      },
+      {
+        picture: `path.jpg`,
+        artist: `Jack Daniels`,
+      },
+      {
+        picture: `path.jpg`,
+        artist: `Jim Beam`,
+      },
+    ],
+  }
+];
