3ea733620b27448577c7e626b1eab9908ff2d71a
demikhovr <demikhovr@gmail.com>
2019-05-12 23:52:59 +0300

    add snapshot tests for ArtistQuestionScreen component

---
+++ src/components/artist-question-screen/artist-question-screen.test.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import ArtistQuestionScreen from './artist-question-screen.jsx';
+
+const mock = {
+  question: {
+    type: `artist`,
+    song: {
+      artist: `Jim Beam`,
+      src: `path.mp3`,
+    },
+    answers: [
+      {
+        picture: `path.jpg`,
+        artist: `John Snow`,
+      },
+      {
+        picture: `path.jpg`,
+        artist: `Jack Daniels`,
+      },
+      {
+        picture: `path.jpg`,
+        artist: `Jim Beam`,
+      },
+    ],
+  },
+};
+
+it(`ArtistQuestionScreen correctly renders`, () => {
+  const {question} = mock;
+  const clickHandler = jest.fn();
+  const tree = renderer
+    .create(<ArtistQuestionScreen
+      question={question}
+      onAnswer={clickHandler}
+    />)
+    .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
