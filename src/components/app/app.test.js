b331156b04da13b0198b1d23c77e45f88d333adc
demikhovr <demikhovr@gmail.com>
2019-05-12 23:46:03 +0300

    add tests for Header component, update tests for App component

--- src/components/app/app.test.js
+++ src/components/app/app.test.js
@@ -2,13 +2,48 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import App from './app.jsx';
 
+const mock = {
+  gameTime: 0,
+  errorCount: 0,
+  questions: [
+    {
+      type: `genre`,
+      genre: `rock`,
+      answers: [
+        {
+          src: `path`,
+          genre: `rock`,
+        },
+      ],
+    },
+    {
+      type: `artist`,
+      song: {
+        artist: `One`,
+        src: ``,
+      },
+      answers: [
+        {
+          picture: ``,
+          artist: `One`,
+        },
+      ],
+    }
+  ],
+};
+
 it(`App correctly renders`, () => {
-  const clickHandler = jest.fn();
+  const {
+    gameTime,
+    errorCount,
+    questions
+  } = mock;
+
   const tree = renderer
     .create(<App
-      gameTime={0}
-      errorCount={0}
-      onWelcomeBtnClick={clickHandler}
+      gameTime={gameTime}
+      errorCount={errorCount}
+      questions={questions}
     />)
     .toJSON();
 
---
+++ src/components/header/header.e2e.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Enzyme, {shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import Header from './header.jsx';
+
+Enzyme.configure({adapter: new Adapter()});
+
+it(`Click on '.game__back' button correctly works`, () => {
+  const clickHandler = jest.fn();
+  const header = shallow(<Header
+    gameTime={7}
+    errorCount={4}
+    onClick={clickHandler}
+  />);
+
+  const backBtn = header.find(`.game__back`);
+  backBtn.simulate(`click`, {preventDefault() {}});
+  expect(clickHandler).toHaveBeenCalledTimes(1);
+});
