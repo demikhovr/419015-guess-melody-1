d004a56b466c6bb99df229a46af37f22d70dd89d
demikhovr <demikhovr@gmail.com>
2019-05-18 09:51:38 +0300

    add snapshot tests for AudioPlayer component

---
+++ src/components/audio-player/audio-player.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import AudioPlayer from './audio-player.jsx';
+
+const mock = {
+  song: {
+    src: `https://upload.wikimedia.org/wikipedia/commons/1/1f/Uganda_flag_and_national_anthem_-_Oh_Uganda_Land_o.ogg`,
+  },
+  isPlaying: false,
+};
+
+it(`AudioPlayer correctly renders`, () => {
+  const {
+    song,
+    isPlaying,
+  } = mock;
+  const clickHandler = jest.fn();
+  const tree = renderer
+    .create(<AudioPlayer
+      src={song.src}
+      isPlaying={isPlaying}
+      onPlayBtnClick={clickHandler}
+    />)
+    .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
