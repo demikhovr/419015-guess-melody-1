791bc0cf152b6bbdceffccf265a96208f2e629e2
demikhovr <demikhovr@gmail.com>
2019-05-20 06:59:56 +0300

    add reducer.js

---
+++ src/store/reducer.js
@@ -0,0 +1,26 @@
+import {
+  INCREMENT_STEP,
+  INCREMENT_MISTAKES,
+} from './actionTypes';
+
+const initialState = {
+  step: -1,
+  mistakes: 0,
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case INCREMENT_STEP:
+      return {
+        ...state,
+        step: state.step + action.payload,
+      };
+    case INCREMENT_MISTAKES:
+      return {
+        ...state,
+        mistakes: state.mistakes + action.payload,
+      };
+    default:
+      return state;
+  }
+};
