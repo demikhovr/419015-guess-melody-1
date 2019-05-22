3cc00f30e4de9ab24633a7216eae6f38267352bd
demikhovr <demikhovr@gmail.com>
2019-05-22 21:31:37 +0300

    init store

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/app/app.jsx";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+import App from './components/app/app.jsx';
 import questions, {settings} from './mocks/questions';
+import reducer from './store/reducer';
+
+const store = createStore(reducer);
 
 const {gameTime, errorCount} = settings;
 
