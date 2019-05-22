8fa7eefd1a1587fe5a0ea6326a478a8dc416d6c7
demikhovr <demikhovr@gmail.com>
2019-05-22 21:33:56 +0300

    wrap App component in Provider

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {createStore} from 'redux';
+import {Provider} from 'react-redux';
 import App from './components/app/app.jsx';
 import questions, {settings} from './mocks/questions';
 import reducer from './store/reducer';
@@ -10,10 +11,12 @@ const store = createStore(reducer);
 const {gameTime, errorCount} = settings;
 
 ReactDOM.render(
-    <App
-      gameTime={gameTime}
-      errorCount={errorCount}
-      questions={questions}
-    />,
+    <Provider store={store}>
+      <App
+        gameTime={gameTime}
+        errorCount={errorCount}
+        questions={questions}
+      />
+    </Provider>,
     document.querySelector(`.main`)
 );
